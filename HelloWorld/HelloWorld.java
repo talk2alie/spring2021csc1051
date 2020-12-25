cdc242e7d445ec79e2b5075079718d68a1cbf037
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2020-12-25 09:16:13 -0500

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * A simple program that prints "Hello, World!" to your screen
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
