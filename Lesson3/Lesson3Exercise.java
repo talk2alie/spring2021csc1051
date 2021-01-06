82ce4d49ff72efd77b82f199f011e82e5289ebd7
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-01-06 18:18:06 -0500

    Add lesson 3 questions

---
+++ Lesson3/Lesson3Exercise.java
@@ -0,0 +1,33 @@
+public class Lesson3Exercise {
+    public static void main(String[] args) {
+        // Predict the Output
+
+        // 1.
+        // System.out.print("I am learning Java.\n");
+        // System.out.println("I will be a great Java developer soon!");
+        // System.out.print("Just watch!");
+
+        // 2.
+        // System.out.print("The Red Devils rule!\r");
+        // System.out.print("The Red Devils rule!\r");
+        // System.out.print("The Red Devils rule!");
+
+        // 3.
+        // System.out.println("Tobin Heath is a legend!/nShe will \'transform\' Manchester United.");
+
+        // Find the error
+        System.out.println();
+
+        // 1.
+        // System.out.println("We are the world.\nWe are the children.");
+
+        // 2.
+        System.out.print("Hearing in the distance"); 
+        System.out.print("Two mandolins like creatures in the"); 
+        System.out.print("dark"); 
+        System.out.print("Creating the agony of ecstasy."); 
+        System.out.println("– George Barker");
+
+        System.out.println();
+    }
+}
---
+++ Lesson3/Smile.java
@@ -0,0 +1,28 @@
+/**
+ * Smile
+ */
+public class Smile {
+    public static void main(String[] args) {
+        System.out.println();
+
+        System.out.println("****\t\t\t\t****");
+        System.out.println(" *   *\t\t\t      *   *");
+        System.out.println("  *   *\t\t\t     *   *");
+        System.out.println("   *   *\t\t    *   *");
+        System.out.println("    *   *\t\t   *   *");
+        System.out.println("     *   *\t\t  *   *");
+        System.out.println("      *   *\t\t *   *");
+        System.out.println("       *   *\t\t*   *");
+        System.out.println("        *   *\t       *   *");
+        System.out.println("         *   *\t      *   *");
+        System.out.println("          *   *\t     *   *");
+        System.out.println("           *   *    *   *");
+        System.out.println("            *   *  *   *");
+        System.out.println("             *   **   *");
+        System.out.println("\t      *      *");
+        System.out.println("\t       *    *");
+        System.out.println("\t\t****");
+
+        System.out.println();
+    }
+}
