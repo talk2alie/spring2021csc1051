3316c39862b7b283c4ebfcc9ec68d8d6e920ba93
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-01-19 18:22:23 -0500

    Add VariableDelaration.java

---
+++ Lesson5/VariableDeclaration.java
@@ -0,0 +1,39 @@
+/**
+ * A simple program that shows how to declare and use variables
+ */
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        System.out.println();
+
+        // Declare variables
+        byte age;
+        short distanceInMiles;
+        int capacity;
+        long annualCapacity;
+
+        byte chairsAvailbe;
+        chairsAvailbe = 28;
+
+        short stadiumAttendees;
+        stadiumAttendees = 32_678;
+
+        int numberOfStudents;
+        numberOfStudents = 28;
+
+        long starsCountedSoFar;
+        starsCountedSoFar = 28L;
+        
+        // Assign values to variables
+        age = 12;
+        distanceInMiles = 2583;
+        capacity = 98586;
+        annualCapacity = capacity * 12;
+        
+        System.out.println("We traveled over " + distanceInMiles + " miles to visit the Machester United Stadium.");
+        System.out.println("It was crowded with over " + capacity + " people. The stadium actually receives up to " );
+        System.out.println(annualCapacity + " visitors a year. The average age of its visitors is " + age + ".");
+        
+        System.out.println();
+    }
+}
+
