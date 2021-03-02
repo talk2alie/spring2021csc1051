38d49f293370bb3172b44c1e867af5ef3eefe161
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-03-02 15:05:50 -0500

    Add Quiz4

---
+++ Lesson4/MyFuture.java
@@ -0,0 +1,30 @@
+public class MyFuture {
+    public static void main(String[] args) {
+        System.out.println();
+
+        String firstName = "Sasudeen";
+        String lastName = "Oketona";
+        String major = "Computer Science";
+        String minor = "Studio Arts";
+        String dreamJob = "architect";
+        int graduationYear = 2022;
+
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println();
+
+        System.out.println("I am studying " + major + " as my major, and " + minor + " as my minor.");
+        System.out.println("When I graduate in " + graduationYear + ", I intend to work in the industry, until I reach an " + dreamJob + " position.");
+
+        System.out.println();
+
+        System.out.println("This is my plan for the future.");
+
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------------------------");
+
+        System.out.println();
+    }
+}
